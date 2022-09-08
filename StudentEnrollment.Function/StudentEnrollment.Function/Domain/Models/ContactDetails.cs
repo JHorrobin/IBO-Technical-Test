7911c8b3280f7b5a59fc05c7801213b9963c3c53
JHorrobin <56409628+JHorrobin@users.noreply.github.com>
2022-09-08 22:55:12 +0100

    Add Models

---
+++ StudentEnrollment.Function/StudentEnrollment.Function/Domain/Models/ContactDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentEnrollment.Function.Domain.Models
+{
+    public class ContactDetails
+    {
+        public string Address { get; set; } // Should be class but string for simplicity
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public PreferredContactMethod PreferredContactMethod { get; set; }
+    }
+}
---
+++ StudentEnrollment.Function/StudentEnrollment.Function/Domain/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StudentEnrollment.Function.Domain.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ContactDetails ContactDetails { get; set; }
+    }
+}
