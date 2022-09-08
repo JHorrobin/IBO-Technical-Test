14303bb61f50fe36349314b2a2f00483fd27f368
JHorrobin <56409628+JHorrobin@users.noreply.github.com>
2022-09-08 22:48:20 +0100

    Initial Commit

---
+++ StudentEnrollment.Function/StudentEnrollment.Function.Tests/StudentEnrollmentFunctionTests.cs
@@ -0,0 +1,11 @@
+namespace StudentEnrollment.Function.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
